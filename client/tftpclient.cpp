74f96948bfdfc9dfa45e5d513fe5bdc5b8e56fb2
Kevin Huang <91703094+HuangK12@users.noreply.github.com>
2021-11-18 14:30:04 -0800

    started implementing buffer building for RRQ/WRQ

--- client/tftpclient.cpp
+++ client/tftpclient.cpp
@@ -9,6 +9,12 @@
 #include <signal.h>         // for the signal handler registration.
 #include <unistd.h>
 
+// to be moved to shared tftp file
+#define RRQ	1
+#define WRQ 2
+#define DATA 3
+#define ACK 4
+#define	ERROR 5
 
 #define SERV_UDP_PORT 51709 //REPLACE WITH YOUR PORT NUMBER
 #define SERV_HOST_ADDR "10.158.82.38" //REPLACE WITH SERVER IP ADDRESS
@@ -65,6 +71,33 @@ int main(int argc, char *argv[])
 		 printf("%s: can't bind local address\n",progname);
 		 exit(2);
 	}
+
+
+    //
+    // working on
+    //
+    char op; // temporary for now, will initialize from arguments
+    char *bufpoint; // for building packet
+    char buffer[512]; // buffer with arbituary 512 size
+    if (op == 'r') {
+        *(short *)buffer = htons(RRQ);
+        bufpoint = buffer + 2; // move pointer to file name
+        strcpy(bufpoint, "test.txt"); // add file name to buffer
+        bufpoint += strlen("test.txt") + 1; //move pointer and add null byte
+        strcpy(bufpoint, "octet"); // add mode to buffer
+        bufpoint += strlen("octet") + 1; // move pointer and add null byte
+    }
+    if (op == 'w') {
+        *(short *)buffer = htons(WRQ);
+        bufpoint = buffer + 2; // move pointer to file name
+        strcpy(bufpoint, "test.txt"); // add file name to buffer
+        bufpoint += strlen("test.txt") + 1; //move pointer and add null byte
+        strcpy(bufpoint, "octet"); // add mode to buffer
+        bufpoint += strlen("octet") + 1; // move pointer and add null byte
+    }
+    //
+    //
+    //
 
 	dg_cli(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr));
 
