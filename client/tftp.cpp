5bf7d9235291238d9b20eaab12f4a3e3d4486f56
Gabriel O <olivergabe@hotmail.com>
2021-11-18 22:05:05 -0800

    setting up a op recognizer in tftp.cpp

--- client/tftp.cpp
+++ client/tftp.cpp
@@ -1,16 +1,34 @@
 //
 // Created by Gabriel Oliver and Kevin Huang on 11/16/2021.
 //
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdlib.h>
+#include <errno.h>          // for retrieving the error number.
+#include <string.h>         // for strerror function.
+#include <signal.h>         // for the signal handler registration.
+#include <unistd.h>
+
+// to be moved to shared tftp file
+#define RRQ	1
+#define WRQ 2
+#define DATA 3
+#define ACK 4
+#define	ERROR 5
+
 
 #include "tftp.h"
 
-void DetermineOP() {
+void DetermineOP(char op) {
 
 
 	//
 	// working on
 	//
-	/*char op; // temporary for now, will initialize from arguments
+	//char op; // temporary for now, will initialize from arguments
 	char *bufpoint; // for building packet
 	char buffer[512]; // buffer with arbituary 512 size
 	if (op == 'r') {
@@ -31,7 +49,7 @@ void DetermineOP() {
 	}
 	//
 	//
-	//*/
+	//
 
 }
 
--- client/tftp.h
+++ client/tftp.h
@@ -4,7 +4,23 @@
 
 #ifndef CSS432KEVINGABRIEL_TFTP_H
 #define CSS432KEVINGABRIEL_TFTP_H
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdlib.h>
+#include <errno.h>          // for retrieving the error number.
+#include <string.h>         // for strerror function.
+#include <signal.h>         // for the signal handler registration.
+#include <unistd.h>
 
+// to be moved to shared tftp file
+#define RRQ	1
+#define WRQ 2
+#define DATA 3
+#define ACK 4
+#define	ERROR 5
 
 void RRQ();
 void WRQ();
