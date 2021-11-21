178e31366e9f64a8605036e9da8e8d5174ce3861
Kevin Huang <91703094+HuangK12@users.noreply.github.com>
2021-11-21 08:27:18 -0800

    cleaning up tftpclient and tftpserver

--- client/tftpclient.cpp
+++ client/tftpclient.cpp
@@ -15,14 +15,78 @@
 #define DATA 3
 #define ACK 4
 #define	ERROR 5
+#define MAXMESG 512
 
 #define SERV_UDP_PORT 51709 //REPLACE WITH YOUR PORT NUMBER
 #define SERV_HOST_ADDR "10.158.82.38" //REPLACE WITH SERVER IP ADDRESS
 
 char *progname;
 
-#define MAXLINE 512
+int main(int argc, char *argv[])
+{
+	if (argc != 3) {
+		printf("%s: invalid number of arguments\n",progname);
+		exit(1);
+    }
+    progname = argv[0];
+    const char *op = argv[1];
+    const char *filename = argv[2];
+    
+    int sockfd;
+	
+	struct sockaddr_in cli_addr, serv_addr;
 
+	bzero((char *) &serv_addr, sizeof(serv_addr));
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_addr.s_addr = inet_addr(SERV_HOST_ADDR);
+	serv_addr.sin_port = htons(SERV_UDP_PORT);
+
+	if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+		printf("%s: can't open datagram socket\n",progname);
+		exit(2);
+	}
+
+	bzero((char *) &cli_addr, sizeof(cli_addr));
+	cli_addr.sin_family = AF_INET;
+	cli_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	cli_addr.sin_port = htons(0);
+
+	if (bind(sockfd, (struct sockaddr *) &cli_addr, sizeof(cli_addr)) < 0) {
+		 printf("%s: can't bind local address\n",progname);
+		 exit(3);
+	}
+
+    char *bufpoint; // for building packet
+    char buffer[MAXMESG]; // packet that will be sent
+    if (op[1] == 'r') {
+        *(short *)buffer = htons(RRQ);
+    }
+    if (op[1] == 'w') {
+        *(short *)buffer = htons(WRQ);
+    }
+    bufpoint = buffer + 2; // move pointer to file name
+    strcpy(bufpoint, filename); // add file name to buffer
+    bufpoint += strlen(filename) + 1; //move pointer and add null byte
+    strcpy(bufpoint, "octet"); // add mode to buffer
+    bufpoint += strlen("octet") + 1; // move pointer and add null byte
+
+    int n = sendto(sockfd, buffer, strlen(buffer), 0, (struct sockaddr *) &serv_addr, sizeof(serv_addr));
+    if (n < 0) {
+		printf("%s: sendto error\n",progname);
+		exit(4);
+	}
+
+    // if RRQ, call tftp shared receiving function 
+    // if WRQ, call tftp shared sending function (may need to receive ACK0 first)
+
+	close(sockfd);
+
+	return 0;
+}
+
+// dg_cli(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr));
+
+/*
 void dg_cli(int sockfd, struct sockaddr *pserv_addr, int servlen) {
 	int n;
 	char sendline[MAXLINE], recvline[MAXLINE + 1];
@@ -44,67 +108,7 @@ void dg_cli(int sockfd, struct sockaddr *pserv_addr, int servlen) {
 		fputs(recvline, stdout);
 	}
 }
-
-int main(int argc, char *argv[])
-{
-	int sockfd;
-	
-	struct sockaddr_in cli_addr, serv_addr;
-	progname = argv[0];
-
-	bzero((char *) &serv_addr, sizeof(serv_addr));
-	serv_addr.sin_family = AF_INET;
-	serv_addr.sin_addr.s_addr = inet_addr(SERV_HOST_ADDR);
-	serv_addr.sin_port = htons(SERV_UDP_PORT);
-
-	if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
-		printf("%s: can't open datagram socket\n",progname);
-		exit(1);
-	}
-
-	bzero((char *) &cli_addr, sizeof(cli_addr));
-	cli_addr.sin_family = AF_INET;
-	cli_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-	cli_addr.sin_port = htons(0);
-
-	if (bind(sockfd, (struct sockaddr *) &cli_addr, sizeof(cli_addr)) < 0) {
-		 printf("%s: can't bind local address\n",progname);
-		 exit(2);
-	}
-
-
-    //
-    // working on
-    //
-    char op; // temporary for now, will initialize from arguments
-    char *bufpoint; // for building packet
-    char buffer[512]; // buffer with arbituary 512 size
-    if (op == 'r') {
-        *(short *)buffer = htons(RRQ);
-    }
-    if (op == 'w') {
-        *(short *)buffer = htons(WRQ);
-    }
-    bufpoint = buffer + 2; // move pointer to file name
-    strcpy(bufpoint, "test.txt"); // add file name to buffer
-    bufpoint += strlen("test.txt") + 1; //move pointer and add null byte
-    strcpy(bufpoint, "octet"); // add mode to buffer
-    bufpoint += strlen("octet") + 1; // move pointer and add null byte
-
-    sendto(sockfd, buffer, strlen(buffer), 0, (struct sockaddr *) &serv_addr, sizeof(serv_addr));
-
-    // if RRQ, call tftp shared receiving function 
-    // if WRQ, call tftp shared sending function
-    //
-    //
-    //
-
-	dg_cli(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr));
-
-	close(sockfd);
-
-	return 0;
-}
+*/
 
 
 
--- server/tftpserver.cpp
+++ server/tftpserver.cpp
@@ -9,12 +9,78 @@
 #include <signal.h>         // for the signal handler registration.
 #include <unistd.h>
 
+// to be moved to shared tftp file
+#define RRQ	1
+#define WRQ 2
+#define DATA 3
+#define ACK 4
+#define	ERROR 5
+#define MAXMESG 512
+
 #define SERV_UDP_PORT   51709 // REPLACE WITH YOUR PORT NUMBER
 
 char *progname;
 
-#define MAXMESG 2048
+int main(int argc, char *argv[]) {
+	if (argc != 1) {
+		printf("%s: invalid number of arguments\n",progname);
+		exit(1);
+    }
+    
+    int sockfd;
 
+	struct sockaddr_in serv_addr;
+
+	progname=argv[0];
+
+	if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+		printf("%s: can't open datagram socket\n",progname);
+		exit(2); 
+	}
+
+	bzero((char *) &serv_addr, sizeof(serv_addr));
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	serv_addr.sin_port = htons(SERV_UDP_PORT);
+
+	if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) { 
+		printf("%s: can't bind local address\n",progname);
+		exit(3);
+	}
+
+	int n, clilen;
+    char buffer[MAXMESG];
+    struct sockaddr pcli_addr;
+
+    for ( ; ; ) {
+        clilen = sizeof(struct sockaddr);
+
+        n = recvfrom(sockfd, buffer, MAXMESG, 0, &pcli_addr, (socklen_t*)&clilen);
+		if (n < 0) {
+			printf("%s: recvfrom error\n",progname);
+			exit(4);
+		}
+
+        unsigned short op = buffer[1];
+
+        if (op == RRQ) {
+            // if RRQ, call tftp shared sending function 
+        }
+        if (op == WRQ) {
+            // if WRQ, send ACK0 and call tftp shared receiving function
+            // BuildAckMessage()
+            // sendto()
+        }
+    }
+
+    return 0;
+}
+
+
+
+// dg_echo(sockfd);
+
+/*
 void dg_echo(int sockfd) {
 	struct sockaddr pcli_addr;
 	int n, clilen;
@@ -37,7 +103,7 @@ void dg_echo(int sockfd) {
 		// after acknowledgement, return to loop
 
 
-/*if (n.OPCode == "RRQ") {
+if (n.OPCode == "RRQ") {
 		    if (sendto(sockfd, mesg, n, 0, &pcli_addr, clilen) != n) {
 		   		printf("%s: sendto error\n",progname);
 		   		exit(4);
@@ -63,7 +129,7 @@ void dg_echo(int sockfd) {
 				}
 		 	}
 
-		 }*/
+		 }
 
 
 
@@ -74,30 +140,4 @@ void dg_echo(int sockfd) {
 		}
 	}
 }
-
-int main(int argc, char *argv[]) {
-	int sockfd;
-
-	struct sockaddr_in serv_addr;
-
-	progname=argv[0];
-
-	if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
-		printf("%s: can't open datagram socket\n",progname);
-		exit(1); 
-	}
-
-	bzero((char *) &serv_addr, sizeof(serv_addr));
-	serv_addr.sin_family = AF_INET;
-	serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-	serv_addr.sin_port = htons(SERV_UDP_PORT);
-
-	if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) { 
-		printf("%s: can't bind local address\n",progname);
-		exit(2);
-	}
-
-	dg_echo(sockfd);
-
-    return 0;
-}
+*/
